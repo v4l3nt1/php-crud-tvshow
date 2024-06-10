0b0f443b331a790d48b4a3f235ad95d5bb16a18e
Valentin Fortier <valentin.fortier@etudiant.univ-reims.fr>
2024-06-10 18:26:02 +0200

    Création méthode d'instance findById dans Poster.php

--- src/Entity/Poster.php
+++ src/Entity/Poster.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 namespace Entity;
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
 
 class Poster{
 
@@ -17,4 +19,21 @@ class Poster{
         return $this->jpeg;
     }
 
+    public static function findById(int $id): Poster
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<SQL
+            SELECT id, jpeg
+            FROM poster
+            WHERE id = :id
+        SQL
+        );
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        if (!($poster = $stmt->fetchObject(Poster::class))) {
+            throw new EntityNotFoundException();
+        } else {
+            return $poster;
+        }
+    }
 }
