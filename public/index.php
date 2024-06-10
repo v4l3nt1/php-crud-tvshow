9898f02060bf539da75710b96b1e5e6c97f8526d
Valentin Fortier <valentin.fortier@etudiant.univ-reims.fr>
2024-06-10 20:06:54 +0200

    Restructuration Html

--- public/index.php
+++ public/index.php
@@ -1,5 +1,29 @@
 <?php
 
 declare(strict_types=1);
+use Entity\Collection\TvShowCollection;
+use Html\AppWebPage;
 
-echo("Hello !");
+$webpage = new AppWebPage();
+$webpage->setTitle('Séries TV');
+
+$tvshows = TvShowCollection::findAll();
+
+$webpage->appendContent("<div class='list'>\n");
+foreach ($tvshows as $tvshow) {
+    $posterId = $tvshow->getPosterId();
+    $webpage->appendContent("            <div class='show'>\n");
+    $webpage->appendContent(<<<HTML
+                                            <div class='poster'>
+                                                <img src='poster.php?posterId=$posterId' alt='Affiche de la série'>
+                                            </div>\n
+                            HTML);
+    $webpage->appendContent("                <p class='name'>{$webpage->escapeString($tvshow->getName())}</p>\n");
+    $webpage->appendContent("                <p class='overview'>{$webpage->escapeString($tvshow->getOverview())}</p>\n");
+    $webpage->appendContent("            </div>\n");
+}
+
+
+$webpage->appendContent("        </div>");
+
+echo $webpage->toHTML();
