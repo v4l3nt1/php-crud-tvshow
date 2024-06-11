cde4bb2a21825ca48d31269ab19c839ed2292172
Valentin Fortier <valentin.fortier@etudiant.univ-reims.fr>
2024-06-11 23:38:50 +0200

    Création de la méthode getHtmlForm de la classe SeasonForm

--- src/Html/Form/SeasonForm.php
+++ src/Html/Form/SeasonForm.php
@@ -29,4 +29,32 @@ class SeasonForm
         return $this->season;
     }
 
+    /** Produit le code html du formulaire SeasonForm
+     * @param string $action
+     * @return string
+     */
+    public function getHtmlForm(string $action): string
+    {
+        $id = $this->season?->getId() ?? '';
+        $tvShowId = $this->escapeString($this->season?->getTvShowId() ?? '');
+        $name = $this->escapeString($this->season?->getName() ?? '');
+        $seasonNumber = $this->escapeString($this->season?->getSeasonNumber() ?? '');
+        $posterId = $this->season?->getPosterId() ?? '';
+
+        $html = <<<HTML
+        <form method="post" action="$action">
+            <input type="hidden" name="id" value="$id">
+            <input type="hidden" name="tvShowId" value="$tvShowId">
+            
+            <label for="name">Nom</label>
+            <input type="text" name="name" value="$name" required>
+            
+            <label for="seasonNumber">Numéro de saison</label>
+            <input type="number" name="seasonNumber" value="$seasonNumber" required>
+            
+            <button type="submit">Enregistrer</button>
+        </form>
+        HTML;
+        return $html;
+    }
 }
