4fa3052ab9dfe36d9f8fc654b3f2b45bfdd353cf
Elias <elias.richarme@etudiant.univ-reims.fr>
2024-06-11 14:38:28 +0200

    première version de season.php

--- public/season.php
+++ public/season.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 use Entity\Exception\EntityNotFoundException;
 use Entity\Season;
+use Entity\TvShow;
 use Html\AppWebPage;
 
 $webpage = new AppWebPage();
+$webpage->setTitle('Episodes de la saison');
 
 if (!isset($_GET["seasonId"]) or !ctype_digit($_GET["seasonId"])) {
     http_response_code(302);
@@ -15,13 +17,42 @@ if (!isset($_GET["seasonId"]) or !ctype_digit($_GET["seasonId"])) {
 }
 
 $webpage = new AppWebPage();
-try {
-    $Episodes = Season::getEpisodes();;
-} catch (EntityNotFoundException $e) {
-    http_response_code(404);
-    exit();
+$season = Season::findById((int)$_GET["seasonId"]);
+$tvShow = TvShow::findById($season->tvShowId);
+$episodes = Season::getEpisodes();
+
+$webpage->appendContent(<<<HTML
+
+                            <div class="info">
+                                <div class="title">
+                                   <p>Série TV : {$tvShow->getName()}</p>
+                                </div>
+                                <div class="season_title">
+                                    <p>$season->getName()</p>
+                                </div>
+                            </div>
+                            <div class="list">
+                               
+                             
+
+
+HTML);
+
+foreach ($episodes as $episode){
+    $webpage->appendContent(<<<HTML
+
+                                <div class="episode">
+                                    <p>{$episode->getEpisodeNumber}</p>
+                                    <p>{$episode->getName}</p>
+                                    <p>{$episode->getOverview}</p>
+                                </div>
+
+HTML);
+
+$webpage->appendContent(<<<HTML
+                            </div>
+HTML
+);
+
+echo $webpage->toHTML();
 }
-
-
-
-
