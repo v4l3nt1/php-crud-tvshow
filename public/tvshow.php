84558eb9789ba65c5dfd3074a1211b292b109358
Valentin Fortier <valentin.fortier@etudiant.univ-reims.fr>
2024-06-11 10:31:00 +0200

    Finalisation page de série tvshow.php

--- public/tvshow.php
+++ public/tvshow.php
@@ -22,15 +22,30 @@ try {
 $webpage->setTitle("{$webpage->escapeString($tvshow->getName())}");
 
 $webpage->appendContent(<<<HTML
-                        <div class="main">
-                            <div class='poster'>
-                                <img src='poster.php?posterId={$tvshow->getPosterId()}' alt='Affiche de la série'>
-                            </div>
-                        </div>                       
-                        <div class='list'>                                              
-                        HTML);
-$seasons = ...;
+<div class="main">
+            <div class='poster'>
+                <img src='poster.php?posterId={$tvshow->getPosterId()}' alt='Affiche de la série'>
+            </div>
+            <div class="info">
+                <div class="name">Titre série : {$webpage->escapeString($tvshow->getName())}</div>
+                <div class="ogName">Titre original : {$webpage->escapeString($tvshow->getOriginalName())}</div>
+                <div class="desc"> Description : <br> {$webpage->escapeString($tvshow->getOverview())}</div>
+            </div>
+        </div>                       
+        <div class='list'>\n
+HTML);
+$seasons = $tvshow->getSeasons();
 foreach ($seasons as $season) {
+    $webpage->appendContent(<<<HTML
+            <div class="season">
+                <div class="seasonPoster">
+                    <img src='poster.php?posterId={$season->getPosterId()}' alt='Affiche de la saison'>
+                </div>
+                <div class="seasonName">
+                    <p>{$webpage->escapeString($season->getName())}</p>
+                </div>
+            </div>\n\n
+HTML);
 }
-$webpage->appendContent("</div>");
+$webpage->appendContent("        </div>");
 echo $webpage->toHTML();
